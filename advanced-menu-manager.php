4a7836727dc3d273179de1f877f2df392671d343
Marin Atanasov <tyxla@abv.bg>
2015-02-21 23:14:33 +0200

    remove plugin uri

--- advanced-menu-manager.php
+++ advanced-menu-manager.php
@@ -1,9 +1,8 @@
 <?php
 /**
  * Plugin Name: Advanced Auto Menu Manager
- * Plugin URI: http://html-lab.com/
  * Description: Allows you to create automatic, fully dynamic menus that can pull any type of content automatically in your menu. Supports posts, pages and any other custom post type; categories, tags and any other custom taxonomy; authors. Supports any menu level and any level of your content hierarchy.
- * Version: 1.0.0
+ * Version: 1.0
  * Author: tyxla
  * Author URI: https://github.com/tyxla
  * License: GPL2
